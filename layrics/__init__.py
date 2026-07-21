9e033a8920111c5826ba874bf53475de9d3aaac5
sitiyou <sitiyou7@gmail.com>
2026-07-22 02:20:47 +0800

    vendor: replace LDDC submodule with vendored copy, strip PySide6

--- layrics/__init__.py
+++ layrics/__init__.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+_vendor = os.path.join(os.path.dirname(__file__), "vendor")
+if os.path.isdir(_vendor) and _vendor not in sys.path:
+    sys.path.insert(0, _vendor)
+
 from ._layrics import ApplicationController
 
 __all__ = ["ApplicationController"]
---
+++ layrics/vendor/LDDC/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: Copyright (C) 2024-2025 沉默の金 <cmzj@cmzj.org>
+# SPDX-License-Identifier: GPL-3.0-only
+
+from .common.version import __version__
+
+__author__ = "沉默の金"
+__license__ = "GPL-3.0-only"
+__copyright__ = "Copyright (C) 2024 沉默の金 <cmzj@cmzj.org>"
+
+__all__ = ["__author__", "__copyright__", "__license__", "__version__"]
